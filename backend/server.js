e52444fd12b9a07e748cd51ae8124a4079c513b7
jeongbaebang <jeongbaebang_dev@naver.com>
2021-11-26 02:14:32 +0900

    [test] 임시 경로수정

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,3 @@ app.use('/', express.static(path.resolve(__dirname, '../src', 'static')));
 app.get('/*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../', 'index.html'));
 });
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log('서버로딩');
-});
