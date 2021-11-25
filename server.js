33bf2528dc3bd56fa29235828063ebfcbb97da83
jeongbaebang <jeongbaebang_dev@naver.com>
2021-11-26 03:20:05 +0900

    [update] heroku 배포 오류 수정중

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const path = require('path');
 
 const app = express();
 
-app.use('/', express.static(path.resolve(__dirname, './', 'static')));
+console.log(__dirname);
+app.use('/', express.static(path.resolve(__dirname, './src/', 'static')));
 
 app.get('/*', (req, res) => {
   res.sendFile(path.resolve(__dirname, './', 'index.html'));
